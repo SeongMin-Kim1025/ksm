79e7197a8e3d1d0f1053834f7bb8fdfa9932fe12
SeongMin-Kim1025 <126821167+SeongMin-Kim1025@users.noreply.github.com>
2024-01-31 16:10:57 +0900

    Add files via upload

---
+++ ksm/ksm/docker-run.sh
@@ -0,0 +1,8 @@
+#
+docker run -it \
+    --rm -p 11345:11345 \
+    --env="DISPLAY" \
+    --env="QT_X11_NO_MITSHM=1" \
+    --volume="/tmp/.X11-unix:/tmp/.X11-unix" \
+    --name=bbb \
+    ksm/local-planner:v1
